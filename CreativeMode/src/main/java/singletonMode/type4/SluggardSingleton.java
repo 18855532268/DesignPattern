990bd8dd70a45b887d56679e8505c758ba5816db
18855532268 <1724868042@qq.com>
2021-05-27 23:05:00 +0800

    新增 新增单例模式懒汉式(线程安全 双重检查)

---
+++ CreativeMode/src/main/java/singletonMode/type4/SluggardSingleton.java
@@ -0,0 +1,40 @@
+package singletonMode.type4;
+
+/**
+ * @author yaoby
+ * @version 1.0
+ * @date 2021/5/24 22:10
+ * <p>
+ * 优缺点  这种确实保证了线程安全 但是效率太低 每个线程在想获得类的实例的时候，都需要等待，同步效率太低
+ */
+public class SluggardSingleton {
+    public static void main(String[] args) {
+        Singleton instance1 = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+        System.out.println(instance1 == instance2);
+        System.out.println(instance1.hashCode());
+        System.out.println(instance2.hashCode());
+
+
+    }
+
+}
+
+// 懒汉式（线程安全 ）
+class Singleton {
+    private static Singleton instance;
+
+    // 构造器私有化
+    private Singleton() {
+
+    }
+    // 本类内部创建对象实例
+
+    public static synchronized Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+
+        return instance;
+    }
+}
---
+++ CreativeMode/src/main/java/singletonMode/type5/SluggardSingleton.java
@@ -0,0 +1,45 @@
+package singletonMode.type5;
+
+/**
+ * @author yaoby
+ * @version 1.0
+ * @date 2021/5/24 22:10
+ * <p>
+ * 优缺点  这种既保证了线程安全，又保证了效率问题 同时解决了懒加载的问题  推荐使用
+ */
+public class SluggardSingleton {
+    public static void main(String[] args) {
+        Singleton instance1 = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+        System.out.println(instance1 == instance2);
+        System.out.println(instance1.hashCode());
+        System.out.println(instance2.hashCode());
+
+
+    }
+
+}
+
+// 懒汉式（线程安全 双重检查）
+class Singleton {
+    private static volatile Singleton instance;
+
+    // 构造器私有化
+    private Singleton() {
+
+    }
+    // 本类内部创建对象实例
+
+    public static Singleton getInstance() {
+
+        if (instance == null) {
+            synchronized (Singleton.class){
+                if (instance == null){
+                    instance = new Singleton();
+                }
+            }
+        }
+
+        return instance;
+    }
+}
